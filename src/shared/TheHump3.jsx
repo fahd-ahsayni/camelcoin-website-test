bc48d8214f465e8816159419b0e1fdb2ac24ef0c
fahd ahsyani <fahdahsayni@gmail.com>
2023-04-06 12:06:07 +0200

    fix hump page

--- src/shared/TheHump3.jsx
+++ src/shared/TheHump3.jsx
@@ -6,8 +6,28 @@ export default function TheHump3() {
   const Images = [hump1, hump1, hump1, hump1];
   return (
     <div>
-      <div className="max-w-2xl z-[109] mx-auto text-center">
-      
+      <div className=" z-[109] mx-auto text-center">
+        <h2 className="mb-4 capitalize text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
+          Revolutionary design
+        </h2>
+        <p className="max-w-xl mx-auto my-4 text-base leading-relaxed text-gray-500 dark:text-gray-300">
+          Have yourself a look on the easy to use and awesome ergonomy of The
+          Hump.
+        </p>
+        <div class="grid mt-20 w-full lg:grid-cols-4 gap-4 justify-items-center md:grid-cols-2">
+          <div>
+            <Image src={hump1} alt="" className="w-40" />
+          </div>
+          <div>
+            <Image src={hump1} alt="" className="w-40" />
+          </div>
+          <div>
+            <Image src={hump1} alt="" className="w-40" />
+          </div>
+          <div>
+            <Image src={hump1} alt="" className="w-40" />
+          </div> 
+        </div>
       </div>
     </div>
   );
